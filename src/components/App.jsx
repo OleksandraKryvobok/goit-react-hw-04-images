851bbc937b321d6b6118544473e62cbaa47ded71
Oleksandra Kryvobok <88559507+OleksandraKryvobok@users.noreply.github.com>
2023-03-23 14:31:13 +0200

    add try/catch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,118 @@
+import { useState, useEffect } from 'react';
+import { Toaster } from 'react-hot-toast';
+
+import { GlobalStyle } from "./GlobalStyle";
+import { Layout, ErrorMessage } from "./Layout";
+
+import ImageGallery from "./ImageGallery";
+import Searchbar from "./Searchbar";
+import Modal from "./Modal";
+import Button from "./Button";
+import Loader from 'components/Loader';
+
+import fetchGallery from 'services/pokemon-api';
+
 export const App = () => {
+  const [searchValue, setSearchValue] = useState('');
+  const [searchResult, setSearchResult] = useState([]);
+  const [imageInfo, setImageInfo] = useState(null);
+  const [page, setPage] = useState(1);
+  const [totalImgs, setTotalImgs] = useState(0);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleSubmit = (searchValue) => {
+    setSearchValue(searchValue);
+    setPage(1);
+    setSearchResult([]);
+    setLoading(true);
+  };
+
+  useEffect(() => {
+    if(searchValue === '') {
+      return;
+    };
+
+    async function fetch() {
+      try {
+        const photos = await fetchGallery(searchValue, page);
+
+        if(photos.hits.length === 0) {
+          return Promise.reject(
+              new Error('There are no images matching. Please try again')
+          );
+        };
+
+        if(page === 1) {
+          setSearchResult(photos.hits);
+        } else {
+          setSearchResult([...searchResult, ...photos.hits]);
+        }
+          
+        setTotalImgs(photos.total);
+        setError(null);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetch();
+
+    // fetchGallery(searchValue, page)
+    // .then(photos => {
+    //   if(photos.hits.length === 0) {
+    //       return Promise.reject(
+    //           new Error('There are no images matching. Please try again')
+    //       );
+    //   };
+
+    //   if(page === 1) {
+    //     setSearchResult(photos.hits);
+    //   } else {
+    //     setSearchResult([...searchResult, ...photos.hits]);
+    //   }
+        
+    //   setTotalImgs(photos.total);
+    //   setError(null);
+    // })
+    // .catch(error => setError(error))
+    // .finally(() => setLoading(false))
+  }, [searchValue, page]);
+
+  const handleBtnClick = () => {
+      setPage(prevState => page + 1);
+  };
+  
+  const toggleModal = (imageInfo) => {
+    setShowModal(showModal => !showModal);
+    setImageInfo(imageInfo);
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Layout>
+        <Toaster />
+        <GlobalStyle />
+        <Searchbar onSearch={handleSubmit} />
+
+        <ImageGallery 
+          searchResult={searchResult} 
+          toggleModal={toggleModal} />
+
+        {loading && <Loader />}
+        {error && <ErrorMessage>{error.message}</ErrorMessage>}
+
+        {searchResult && 
+        searchResult.length < totalImgs && 
+        !error && 
+        <Button onBtnClick={handleBtnClick} />}
+
+        {showModal && 
+        <Modal onClose={toggleModal}>
+          <img src={imageInfo.largeImageURL} alt={imageInfo.tags} />    
+        </Modal>}
+    </Layout>
   );
 };
